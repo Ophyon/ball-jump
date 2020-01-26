295af6b1f3180d14c0c674c43c93a1fb83fa64bc
TootNoot <54513686+TootNoot@users.noreply.github.com>
2020-01-26 14:15:25 -0500

    Add files via upload

---
+++ index js.js
@@ -0,0 +1,31 @@
+
+        function changeCheck1() {
+            var x = document.getElementById("levelCheck1");
+            if (x.innerHTML === "❎") {
+                x.innerHTML = `✅`;
+            }
+        }
+        function changeCheck2() {
+            var x = document.getElementById("levelCheck2");
+            if (x.innerHTML === "❎") {
+                x.innerHTML = `✅`;
+            }
+        }
+        function changeCheck3() {
+            var x = document.getElementById("levelCheck3");
+            if (x.innerHTML === "❎") {
+                x.innerHTML = `✅`;
+            }
+        }
+        function changeCheck4() {
+            var x = document.getElementById("levelCheck4");
+            if (x.innerHTML === "❎") {
+                x.innerHTML = `✅`;
+            }
+        }
+        function changeCheck5() {
+            var x = document.getElementById("levelCheck5");
+            if (x.innerHTML === "❎") {
+                x.innerHTML = `✅`;
+            }
+        }
